5de0febeb98d296e15d54e6ca4584d33a28f40fe
Mozart <mozartcomart@gmail.com>
2021-03-09 21:49:35 -0300

    Adicionando material design bootstrap

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/mdb.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
