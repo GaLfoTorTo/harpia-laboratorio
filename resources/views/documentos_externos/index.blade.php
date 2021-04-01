e4d9e6b14121ec86297d80528b14debd2245b1a8
Porfirio2002 <pedrohenriquedasilva014@gmail.com>
2021-04-01 18:59:02 -0300

    01/04/2021

--- resources/views/documentos_externos/index.blade.php
+++ resources/views/documentos_externos/index.blade.php
@@ -1,33 +1,57 @@
 @include('layout.header')
-@include('layout.menu')
+@include('layout.navbar')
+@include('layout.sidebar')
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Documentos Externos</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item "><a href="/">Dashboard</a></li>
+              <li class="breadcrumb-item active">Documentos Externos</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
 
 <div class="container corpo">
   <h2>Documentos Externos</h2>
 
-  <div class="row">
-      <div class="col">
-          <a href="/documentos_externos/novo" class="btn btn-primary">
-            Novo Documento
-            <i class="fas fa-plus"></i>
-          </a>
-      </div>
-      <div class="col ">
-        <form action="">
-          <div class="input-group justify-content-end">
-            <div class="form-outline ">
-              <input type="search" id="form1" class="form-control" name="pesquisa"/>
-              <label class="form-label" for="form1">Pesquisar</label>
+  <section class="content">
+      <div class="container-fluid">
+
+  <div class="card">
+          <div class="card-header">
+            <a href="/documentos_externos/novo" class="btn btn-primary">
+              Novo documento 
+              <i class="fas fa-plus"></i>
+            </a>
+
+            <div class="card-tools">
+              <form action="">
+                <div class="input-group input-group" style="width: 150px;">
+                <input type="search" id="form1" class="form-control" name="pesquisa"/>
+              <label class="form-label" for="form1"></label>
+                  <div class="input-group-append">
+                    <button type="submit" class="btn btn-default">
+                      <i class="fas fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
-            <button type="button" class="btn btn-primary">
-              <i class="fas fa-search"></i>
-            </button>
           </div>
-        </form>
-    </div>
-  </div>
-  <div class="row">
-      <div class="col">
-          <table class="table table-bordered table-hover">
+          <!-- /.card-header -->
+          <div class="card-body table-responsive p-0">
+            <table class="table table-hover text-nowrap table-bordered ">
               <thead>
                 <tr>
                     <th scope="col">#</th>
