0b7a540e2d527722cc726e086b9f428be2d164ef
Mozart <mozartcomart@gmail.com>
2021-04-12 21:13:53 -0300

    Inserindo notificações

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/" class="brand-link">
       <img src="/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Harpia Laboratório</span>
     </a>
@@ -14,7 +14,7 @@
           <img src="/img/user-default.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Admin</a>
+          <a href="/" class="d-block">Admin</a>
         </div>
       </div>
 
@@ -24,7 +24,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="#" class="nav-link active">
+            <a href="/" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
@@ -41,7 +41,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href/" class="nav-link">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Clientes
@@ -64,7 +64,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href/" class="nav-link">
               <i class="nav-icon fas fa-user-friends"></i>
               <p>
                 Colaboradores
@@ -87,7 +87,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href/" class="nav-link">
               <i class="nav-icon far fa-copy"></i>
               <p>
                 Documentos Externos
@@ -110,7 +110,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href/" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
                 Documentos Internos
@@ -133,7 +133,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href/" class="nav-link">
               <i class="nav-icon fas fa-vial"></i>
               <p>
                 Equipamentos
